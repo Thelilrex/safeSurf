6a92f37f6f1e03097f20f24dc62bb995682f88b6
Thelilrex <137387550+Thelilrex@users.noreply.github.com>
2024-10-03 12:46:38 +0900

    added a test case to popup.js and changes to manifest

--- js/popup.js
+++ js/popup.js
@@ -1,4 +1,5 @@
 let restrict = ["x.com"];
 if (window.location.host === "www.youtube.com") {
-  alert("bruh");
+  document.body.innerHTML =
+    "<img src= 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQFidw-FrZFP8uFFYSv5ST1NS1j74NieSJEZA&s' >";
 }
