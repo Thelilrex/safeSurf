995f446c98d43b11287a90ce78c21b17344102dd
Thelilrex <137387550+Thelilrex@users.noreply.github.com>
2024-10-03 14:27:51 +0900

    added the navigation from stats button to home page

--- js/popup.js
+++ js/popup.js
@@ -3,3 +3,11 @@ if (window.location.host === "www.youtube.com") {
   document.body.innerHTML =
     "<img src= 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQFidw-FrZFP8uFFYSv5ST1NS1j74NieSJEZA&s' >";
 }
+document.addEventListener("DOMContentLoaded", function () {
+  const re_direct_el = document.getElementById("re_direct"); // Make sure the ID matches
+  re_direct_el.addEventListener("click", function () {
+    chrome.tabs.create({
+      url: chrome.runtime.getURL("../html/home.html"),
+    });
+  });
+});
